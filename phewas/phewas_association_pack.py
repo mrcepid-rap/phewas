0d72b370800f00f5179767aa68e6e0d4a318cbc8
ejgardner_adrestia <ejgardner@adrestia.com>
2023-03-10 09:40:54 +0000

    Updating prior version control and test implementation

--- phewas/phewas_association_pack.py
+++ phewas/phewas_association_pack.py
@@ -19,11 +19,9 @@ class PhewasAssociationPack(AssociationPack):
     def __init__(self, association_pack: AssociationPack,
                  is_snp_tar: bool, is_gene_tar: bool, tarball_prefixes: List[str], gene_ids: List[str]):
 
-        super().__init__(association_pack.pheno_files, association_pack.inclusion_found,
-                         association_pack.exclusion_found, association_pack.additional_covariates_found,
-                         association_pack.is_binary, association_pack.sex, association_pack.threads,
-                         association_pack.pheno_names,
-                         association_pack.found_quantitative_covariates, association_pack.found_categorical_covariates)
+        super().__init__(association_pack.is_binary, association_pack.sex, association_pack.threads,
+                         association_pack.pheno_names, association_pack.found_quantitative_covariates,
+                         association_pack.found_categorical_covariates)
 
         self.is_snp_tar = is_snp_tar
         self.is_gene_tar = is_gene_tar
